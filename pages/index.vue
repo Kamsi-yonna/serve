b45b9ee136ac5e576875d4f3499d31e68be61be2
Kamsiyonna <etiobikamsi@gmail.com>
2024-08-21 15:27:43 +0100

    feat - page setup

---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div v-if="data">
+      <h1>Random Dog</h1>
+      <img :src="data" alt="Random Dog" />
+      {{ data }}
+    </div>
+    <div v-else>
+      <p>Loading...</p>
+    </div>
+    <button @click="refetch">Fetch New Dog</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+// import { NuxtImg } from '#build/components';
+import useRandomDog from '@/composables/useRandomDog';
+
+const { data, isLoading, refetch } = useRandomDog();
+</script>
+
+<style scoped>
+img {
+  border: 1px solid #ddd;
+  border-radius: 4px;
+  padding: 5px;
+  width: 200px;
+}
+</style>

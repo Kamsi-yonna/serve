731b4660595af12521e8bbcc0161da88bbbb300f
Kamsiyonna <etiobikamsi@gmail.com>
2024-08-26 00:43:22 +0100

    feat - setup tailwind prefixer

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,15 +1,14 @@
-import type { Config } from 'tailwindcss'
-
-export default <Partial<Config>>{
+export default {
   content: [
     './components/**/*.{js,vue,ts}',
     './layouts/**/*.vue',
     './pages/**/*.vue',
     './plugins/**/*.{js,ts}',
     './nuxt.config.{js,ts}',
-    './app.vue'
+    './app.vue',
   ],
   theme: {
-    extend: {}
+    extend: {},
   },
-}
+  plugins: [],
+};
